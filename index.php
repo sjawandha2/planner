be42bf14fa9e3f27e448f9168714bd4bc904da41
carmen_fresh_ga@hotmail.com <carmen_fresh_ga@hotmail.com>
2020-03-08 17:19:53 -0700

    add a separate route for the planner

--- index.php
+++ index.php
@@ -72,6 +72,19 @@ $f3->route('GET|POST /', function ($f3) {
             $country = $characters->sys->country;
             $lon = $characters->coord->lon;
             $lat = $characters->coord->lat;
+            $limit =5;
+
+            //Store all the information to the session and fat-free
+            $_SESSION['country'] = $country;
+            $_SESSION['weather'] = $weather;
+            $_SESSION['windSpeed'] = $windSpeed;
+            $_SESSION['temperature'] = $temperature;
+            $_SESSION['maxtemp'] = $maxtemp;
+            $_SESSION['mintemp'] = $mintemp;
+            $_SESSION['humidity'] = $humidity;
+            $_SESSION['pressure'] = $pressure;
+            $_SESSION['sunset'] = $sunset;
+            $_SESSION['sunrise'] = $sunrise;
 
             $f3->set('city', $city);
             $f3->set('country', $country);
@@ -85,47 +98,56 @@ $f3->route('GET|POST /', function ($f3) {
             $f3->set('sunset', $sunset);
             $f3->set('sunrise', $sunrise);
 
-//all the attributes to send an api request to opentripmap.com
+            //all the attributes to send an api request to opentripmap.com
 
-//            $eventsKey = '5ae2e3f221c38a28845f05b62269320ee1152b3977ead5ffe690f1c5';
-////kinds can be separated by a comma to send a list of type of places
-//            $language = "en";
-//
-////url containing all attributes
-//            $eventsUrl = "https://api.opentripmap.com/0.1/$language/places/radius?radius=$radius&lon=$lon&lat=$lat&kinds=$kinds&apikey=$eventsKey";
-//
-////print the response
-//            $allEvents = file_get_contents($eventsUrl);
-//            $details = json_decode($allEvents);
-//
-//            if ($details) {
-//                echo "Here is a list of suggested $kinds: <br>";
-//                for ($x = 0; $x < sizeof($details->features); $x++) {
-//                   // echo $details->features[$x]->properties->name . "<br>";
-//
-//
-//                    $info = $details->features[$x]->properties->name . "<br>";
-//
-//
-//                    //retrieve the object id
-//                    $xid = $details->features[$x]->properties->xid;
-//                    $objectUrl = "https://api.opentripmap.com/0.1/$language/places/xid/$xid?apikey=$eventsKey";
-//                    $objectResponse = json_decode(file_get_contents($objectUrl));
-//                    $image = $objectResponse->preview->source;
-//                    $infoImg ='<img src="' . $image . '" alt="Image not found" scale="0"><br><br>';
-//
-//                    $f3->set('info', $info);
-//                    $f3->set('infoImg', $infoImg);
-//                }
-//            } else {
-//                echo "Try a larger distance than $radius meters";
-//            }
+            $eventsKey = '5ae2e3f221c38a28845f05b62269320ee1152b3977ead5ffe690f1c5';
+            $language = "en";
 
-//        }
-//        else {
-//            echo "Please enter a valid city";
-            $view = new Template();
-            echo $view->render('views/info.html');
+            //url containing all attributes
+            $eventsUrl = "https://api.opentripmap.com/0.1/$language/places/radius?radius=$radius&lon=$lon&lat=$lat&limit=$limit&kinds=$kinds&apikey=$eventsKey";
+            $allEvents = file_get_contents($eventsUrl);
+            $details = json_decode($allEvents);
+            $imageArray=[];
+            $infoArray =[];
+
+            if ($details) {
+                //                echo "Here is a list of suggested $kinds: <br>";
+                for ($x = 0; $x < sizeof($details->features); $x++) {
+
+                    // echo $details->features[$x]->properties->name . "<br>";
+                    //
+
+                    $name = $details->features[$x]->properties->name;
+                    //retrieve the object id for each object
+                    $xid = $details->features[$x]->properties->xid;
+
+                    //retrieve the object images for each object making a call  by object id to opentripmap
+                    $objectUrl = "https://api.opentripmap.com/0.1/$language/places/xid/$xid?apikey=$eventsKey";
+                    $objectResponse = json_decode(file_get_contents($objectUrl));
+                    $image = $objectResponse->preview->source;
+
+                    //$infoImg = '<img src="' . $image . '" alt="Image not found" scale="0"><br><br>';
+
+
+                    array_push($imageArray,$image);
+                    array_push($infoArray,$name);
+
+
+                }//for loop
+
+                //save all the images and information into fat-free and the session
+                $f3->set('imageArray',$imageArray);
+                $f3->set('infoArray', $infoArray);
+
+                $_SESSION['imageArray']=$imageArray;
+                $_SESSION['infoArray']=$infoArray;
+            }//if details
+
+            else {
+
+                echo "Try a larger distance than $radius miles";
+            }
+            $f3->reroute('info');
         }
 
     }
@@ -135,8 +157,48 @@ $f3->route('GET|POST /', function ($f3) {
     echo $view->render('views/planner.html');
 });
 
+//Define a default route
+$f3->route('GET|POST /info', function ($f3) {
+    if(!empty($_POST)){
+        session_unset();
+        $f3->reroute('/');
+    }
 
+    //get all the information from the session
+    $city = $_SESSION['city'];
+    $country = $_SESSION['country'];
+    $weather = $_SESSION['weather'];
+    $windSpeed = $_SESSION['windSpeed'];
+    $temperature = $_SESSION['temperature'];
+    $maxtemp = $_SESSION['maxtemp'];
+    $mintemp = $_SESSION['mintemp'];
+    $humidity = $_SESSION['humidity'];
+    $pressure = $_SESSION['pressure'];
+    $sunset = $_SESSION['sunset'];
+    $sunrise = $_SESSION['sunrise'];
+    $imageArray = $_SESSION['imageArray'];
+    $infoArray = $_SESSION['infoArray'];
+
+    //store all the information in f3 token
+    $f3->set('city', $city);
+    $f3->set('country', $country);
+    $f3->set('weather', $weather);
+    $f3->set('windSpeed', $windSpeed);
+    $f3->set('temperature', $temperature);
+    $f3->set('maxtemp', $maxtemp);
+    $f3->set('mintemp', $mintemp);
+    $f3->set('humidity', $humidity);
+    $f3->set('pressure', $pressure);
+    $f3->set('sunset', $sunset);
+    $f3->set('sunrise', $sunrise);
+    $f3->set('imageArray',$imageArray);
+    $f3->set('infoArray', $infoArray);
+
+
+    //return to the main page, reset the session
+    $view = new Template();
+    echo$view->render('views/info.html');
+});
 
 //Run Fat-Free
 $f3->run();
-
